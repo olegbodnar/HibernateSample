e502933b63d1bf67c8305a04c533267c0d91da40
expensve <mikeyg@ukr.net>
2015-04-20 21:49:50 +0300

    examples that I dont understand

--- Task32/src/Main.java
+++ Task32/src/Main.java
@@ -1,13 +1,28 @@
 
 public class Main {
 
-    public static void main(String[] args) {
-        Integer number = 2015;
+//    public static void main(String[] args) {
+//        Integer number = 2015;
+//
+//        aRR aRR = new aRR(number);
+//        aRR.getDigits();
+public static void main(String[] args) {
+    int [] arr = {1,2,3,4,5};
+    int result = 0;
 
-        aRR aRR = new aRR(number);
-        aRR.getDigits();
-
-
-
+    for (int i =arr.length -1 , f = 0; i >= 0; --i, f++) {
+        int pos = (int)Math.pow(10, i);
+        result += arr[f] * pos;
     }
+    System.out.println(result);
+}
+    int i = 0;
+    while (num > 0)
+    {
+        digits[i++] = num % 10;
+        num /= 10;
+    }
+
+
+
 }
--- Task32/src/aRR.java
+++ Task32/src/aRR.java
@@ -1,24 +1,24 @@
 
-public class aRR {
-    private Integer[] digits;
-    private Integer length;
-
-    public aRR(Integer[] digits, Integer[] length) {
-        this.digits = digits;
-        this.length = digits.length;
-    }
-
-    Integer digit[];
-    Integer part;
-
-    public aRR(Integer number) {
-    }
-
-    public void getDigits() {
-        for (part = 0; part < length; part++) {
-            digit[part] = part;
-
-        }
-        System.out.println(part);
-    }
-}
+//public class aRR {
+//    private Integer[] digits;
+//    private Integer length;
+//
+//    public aRR(Integer[] digits, Integer[] length) {
+//        this.digits = digits;
+//        this.length = digits.length;
+//    }
+//
+//    Integer digit[];
+//    Integer part;
+//
+//    public aRR(Integer number) {
+//    }
+//
+//    public void getDigits() {
+//        for (part = 0; part < length; part++) {
+//            digit[part] = part;
+//
+//        }
+//        System.out.println(part);
+//    }
+//}
